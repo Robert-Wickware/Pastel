2ff7381af809653514d5296195ee7acca3180eff
Robert Evola <robert@wickware.com>
2015-06-30 15:34:04 -0400

    Basic connection to database established

--- app.js
+++ app.js
@@ -2,22 +2,35 @@
  
 var express = require('express');
 var path = require('path');
-
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://wickware-admin:R2-pakouhite@ds051831.mongolab.com:51831/heroku_mlcfqv1f');
-if(mongoose.connection != null) {
-  console.log("Connected to database!");
-}
-
-
-var routes = require(path.join(__dirname, 'routes/index'));
+var uriUtil = require('mongodb-uri');
 var app = express();
+
+//Alows for serving files such as CSS and JS
+app.use(express.static(__dirname + '/public'));
+
+//Converting the mongoDB URI to a compatible mongoose URI
+var mongodbURI = 'mongodb://wickware-admin:R2-pakouhite@ds051831.mongolab.com:51831/heroku_mlcfqv1f';
+var mongooseURI = uriUtil.formatMongoose(mongodbURI);
+
+//Attempt to connect to the DB, save the connection 
+mongoose.connect(mongooseURI);
+var db = mongoose.connection;
+
+//For now, print out the error if one occurs or notify a connection has been established
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log("Connection open!");
+});
+
+//Creating the path's for the routes
+var indexRoute = require(path.join(__dirname, 'routes/index'));
 
 //Set location of the views as well as which Rendering Engine to use
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'me2');
 
-app.use('/', routes);
+app.use('/', indexRoute);
  
 //Use the appropriate port if running on Heroku or Locally
 var port = Number(process.env.PORT || 9292);
