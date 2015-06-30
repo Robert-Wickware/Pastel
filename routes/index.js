9cf5a2c821ce6b8a44f9f0eab949fc3605ad4332
Robert Evola <robert@wickware.com>
2015-06-30 13:09:16 -0400

    Fixing issue with routing

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 /// <reference path="../typings/tsd.d.ts" />
 var express = require('express');
-var app = express();
+var router = express.Router();
 
-app.get('/', function (req, res) {
+router.get('/', function (req, res) {
   res.render('index', {name: 'Rob Evola', job: 'Programmer'});
 });
