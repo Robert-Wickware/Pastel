1854fb309d8dfb0e37c1c1a3abcc963cb8191cc5
Robert Evola <robert@wickware.com>
2015-07-20 10:01:47 -0400

    Recording Area Structure

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,9 +1,60 @@
 /// <reference path="../../typings/tsd.d.ts" />
 
 var main = function() {
-	var name = $('#name').text();
-	name += '!!!';
-	$('#name').text(name);
+	
+	$("#script-work-btn").click(function() {
+		var scriptBtn = $("#script-work-btn");
+		var activeBtn = scriptBtn.siblings(".active");
+		
+		activeBtn.removeClass("active");
+		scriptBtn.addClass("active");
+		
+		var activeComponent = $("#work-component-panel").children(".active");
+		var scriptComponent = $("#script-component-panel");
+		
+		activeComponent.removeClass("active");
+		activeComponent.hide();
+		
+		scriptComponent.addClass("active");
+		scriptComponent.show();
+	});
+	
+	$("#record-work-btn").click(function() {
+		var recordBtn = $("#record-work-btn");
+		var activeBtn = recordBtn.siblings(".active");
+		
+		activeBtn.removeClass("active");
+		recordBtn.addClass("active");
+		
+		var activeComponent = $("#work-component-panel").children(".active");
+		var recordComponent = $("#record-component-panel");
+		
+		activeComponent.removeClass("active");
+		activeComponent.hide();
+		
+		recordComponent.addClass("active");
+		recordComponent.show();
+	});
+	
+	$("#edit-work-btn").click(function() {
+		var editBtn = $("#edit-work-btn");
+		var activeBtn = editBtn.siblings(".active");
+	
+		activeBtn.removeClass("active");
+		editBtn.addClass("active");
+		
+		var activeComponent = $("#work-component-panel").children(".active");
+		var editComponent = $("#edit-component-panel");
+		
+		activeComponent.removeClass("active");
+		activeComponent.hide();
+		
+		editComponent.addClass("active");
+		editComponent.show();
+	});
+	
+	$("#record-component-panel").hide();
+	$("#edit-component-panel").hide();
 	
 	var player = videojs('viewport');
 	player.ready(function() {
